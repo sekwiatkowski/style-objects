84fe3328334e47ed0dfcb99ee09c8f9dfcca46d4
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-09-26 05:51:59 +0200

    shadows: added missing exports

--- src/shadows.js
+++ src/shadows.js
@@ -1,10 +1,10 @@
 import {joinWithCommaSpace, joinWithSpace} from 'compose-functions'
 
-function shadow(offsetX, offsetY, blur, spreadRadius, color) {
+export function shadow(offsetX, offsetY, blur, spreadRadius, color) {
     return joinWithSpace([offsetX, offsetY, blur, spreadRadius, color])
 }
 
-function boxShadow(shadows) {
+export function boxShadow(shadows) {
     return {
         boxShadow: joinWithCommaSpace(shadows)
     }
