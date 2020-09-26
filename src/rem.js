de013317fe40b3f2cab080a319543cf6b9a322a5
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-09-26 16:21:02 +0200

    rem: made rems public again

--- src/rem.js
+++ src/rem.js
@@ -1,7 +1,7 @@
 import {toString} from 'compose-functions'
 import {eights, quarters} from './fractions'
 
-function rems(units) {
+export function rems(units) {
     return toString(units) + 'rem'
 }
 
