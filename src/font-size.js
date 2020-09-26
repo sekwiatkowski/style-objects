b99e5dc4d55c971ed058f48f80bf42095e50d035
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-09-26 16:56:56 +0200

    font-size: added a style for font sizes of 9/8th REMs

--- src/font-size.js
+++ src/font-size.js
@@ -8,6 +8,8 @@ export const smallerFontSize = eighthRemFontSize(7)
 
 export const normalFontSize = eighthRemFontSize(8)
 
+export const slightlyLargerFontSize = eighthRemFontSize(9)
+
 export const largerFontSize = eighthRemFontSize(10)
 
 export const largeFontSize = eighthRemFontSize(12)
