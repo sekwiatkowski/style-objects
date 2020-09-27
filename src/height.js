18122cb85e6e53af5f45cea44a3662ac29939c56
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-09-27 22:03:28 +0200

    height: added functions for heights between 40 and 96 quarter REMs in steps of 8

--- src/height.js
+++ src/height.js
@@ -1,3 +1,5 @@
+import {quarterRemStyle} from './base-unit'
+
 function height(height) {
     return { height }
 }
@@ -6,6 +8,17 @@ function height(height) {
 function minHeight(minHeight) {
     return { minHeight }
 }
+
+export const quarterRemHeight = quarterRemStyle('height')
+
+export const height40 = quarterRemHeight(40)
+export const height48 = quarterRemHeight(48)
+export const height56 = quarterRemHeight(56)
+export const height64 = quarterRemHeight(64)
+export const height72 = quarterRemHeight(72)
+export const height80 = quarterRemHeight(80)
+export const height88 = quarterRemHeight(88)
+export const height96 = quarterRemHeight(96)
 
 export const fullHeight = height('100%')
 export const fullMinHeight = minHeight('100%')
