1cc5a3a2207c9e2381917ff42288cf5fdcbb5bc3
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-09-28 03:35:26 +0200

    alignment: renamed selfCenter to alignSelfCenter, added constants to set align-items, justify-content and justify-self to "center"

--- src/alignment.js
+++ src/alignment.js
@@ -1,6 +1,19 @@
-export const selfCenter = {
-    'alignSelf': 'center'
+export const alignItemsCenter = {
+    alignItems: 'center'
 }
+
+export const alignSelfCenter = {
+    alignSelf: 'center'
+}
+
+export const justifyContentCenter = {
+    justifyContent: 'center'
+}
+
+export const justifySelfCenter = {
+    justifySelf: 'center'
+}
+
 
 export const center = {
     marginLeft: 'auto',
