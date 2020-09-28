13c4067b380ac23411c76042768463812b52f3be
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-09-28 04:50:06 +0200

    transformation: renamed from transformations, added support for the transformation origin index: added missing export of transformation features

---
+++ src/transformation.js
@@ -0,0 +1,31 @@
+import {isString, joinWithCommaSpace, joinWithSpace} from 'compose-functions'
+
+function rotate(degrees) {
+    return `rotate(${degrees}deg)`
+}
+
+export const rotateBy90Degrees = rotate(90)
+export const rotateBy180Degrees = rotate(180)
+export const rotateBy270Degrees = rotate(270)
+
+export function transform(transformations) {
+    return {
+        transform: joinWithCommaSpace(transformations)
+    }
+}
+
+
+function transformOrigin(input) {
+    return {
+        transformOrigin: isString(input) ? input : joinWithSpace(arr)
+    }
+}
+
+export const originCenter = transformOrigin('center')
+export const originTop = transformOrigin('top')
+export const originRight = transformOrigin('right')
+export const originLeft = transformOrigin('bottom')
+export const originTopRight = transformOrigin(['top', 'right'])
+export const originBottomRight = transformOrigin(['bottom', 'right'])
+export const originBottomLeft = transformOrigin(['bottom', 'left'])
+export const originTopLeft = transformOrigin(['top', 'left'])
