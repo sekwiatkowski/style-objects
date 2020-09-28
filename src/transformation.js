d063e1e6abafc636d4262c4220eff651c9ca0f28
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-09-28 06:10:45 +0200

    position: removed completely transform: removed rotation by 270 degrees and code related to the transform origin

--- src/transformation.js
+++ src/transformation.js
@@ -1,4 +1,4 @@
-import {isString, joinWithCommaSpace, joinWithSpace} from 'compose-functions'
+import {joinWithCommaSpace} from 'compose-functions'
 
 function rotate(degrees) {
     return `rotate(${degrees}deg)`
@@ -6,26 +6,9 @@ function rotate(degrees) {
 
 export const rotateBy90Degrees = rotate(90)
 export const rotateBy180Degrees = rotate(180)
-export const rotateBy270Degrees = rotate(270)
 
 export function transform(transformations) {
     return {
-        transform: joinWithCommaSpace(transformations)
+        transform: Array.isArray(transformations) ? joinWithCommaSpace(transformations) : input
     }
 }
-
-
-function transformOrigin(input) {
-    return {
-        transformOrigin: isString(input) ? input : joinWithSpace(input)
-    }
-}
-
-export const originCenter = transformOrigin('center')
-export const originTop = transformOrigin('top')
-export const originRight = transformOrigin('right')
-export const originLeft = transformOrigin('bottom')
-export const originTopRight = transformOrigin(['top', 'right'])
-export const originBottomRight = transformOrigin(['bottom', 'right'])
-export const originBottomLeft = transformOrigin(['bottom', 'left'])
-export const originTopLeft = transformOrigin(['top', 'left'])
