138e02e2fd1d77810c0a37c58824ba92cae84c81
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-09-28 04:16:05 +0200

    index: added missing export of everything from the position.js file position: added objects to position elements at the zero top/right/bottom/left position

--- src/position.js
+++ src/position.js
@@ -5,3 +5,12 @@ function position(position) {
 export const absolute = position('absolute')
 
 export const relative = position('relative')
+
+function zero(side) {
+    return {[side]: '0'}
+}
+
+export const top0 = zero('top')
+export const right0 = zero('right')
+export const bottom0 = zero('bottom')
+export const left0 = zero('left')
