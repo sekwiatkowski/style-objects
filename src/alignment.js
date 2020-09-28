56ead203fc6ad90f9f66ef5aaee661a713917073
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-09-28 22:13:26 +0200

    writing-mode: added the vertical-rl writing mode alignment: refactored using factory functions, added objects with the flex-end setting

--- src/alignment.js
+++ src/alignment.js
@@ -1,19 +1,30 @@
-export const alignItemsCenter = {
-    alignItems: 'center'
+function alignItems(alignItems) {
+    return { alignItems }
 }
 
-export const alignSelfCenter = {
-    alignSelf: 'center'
+export const alignItemsCenter = alignItems('center')
+export const alignItemsFlexEnd = alignItems('flex-end')
+
+function alignSelf(alignSelf) {
+    return { alignSelf }
 }
 
-export const justifyContentCenter = {
-    justifyContent: 'center'
+export const alignSelfCenter = alignSelf('center')
+export const alignSelfFlexEnd = alignSelf('flex-end')
+
+function justifyContent(justifyContent) {
+    return { justifyContent }
 }
 
-export const justifySelfCenter = {
-    justifySelf: 'center'
+export const justifyContentCenter = justifyContent('center')
+export const justifyContentFlexEnd = justifyContent('flex-end')
+
+function justifySelf(justifySelf) {
+    return { justifySelf }
 }
 
+export const justifySelfCenter = justifySelf('center')
+export const justifySelfFlexEnd = justifySelf('flex-end')
 
 export const center = {
     marginLeft: 'auto',
