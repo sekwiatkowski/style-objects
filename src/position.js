e3ae9fb975a555a3acbce46ce32eb7376a7fd776
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-10-02 03:55:46 +0200

    position: added style object to position elements on any of the edges of the parent

--- src/position.js
+++ src/position.js
@@ -5,3 +5,26 @@ function position(position) {
 export const relative = position('relative')
 export const absolute = position('absolute')
 
+function top(left) {
+    return { left }
+}
+
+export const top0 = top('0')
+
+function right(right) {
+    return { right }
+}
+
+export const right0 = right('0')
+
+function bottom(bottom) {
+    return { bottom }
+}
+
+export const bottom0 = bottom('0')
+
+function left(left) {
+    return { left }
+}
+
+export const left0 = left('0')
