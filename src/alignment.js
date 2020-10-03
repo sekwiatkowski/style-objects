5f7308431ba7b0a70e64471067eddbe0d3ee5188
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-10-03 22:05:59 +0200

    alignment: refactored centerText using the new textAlign function and added justifyText

--- src/alignment.js
+++ src/alignment.js
@@ -30,6 +30,10 @@ export const justifySelfCenter = justifySelf('center')
 export const justifySelfFlexStart = justifySelf('flex-start')
 export const justifySelfFlexEnd = justifySelf('flex-end')
 
-export const centerText = {
-    textAlign: 'Center'
+function textAlign(textAlign) {
+    return { textAlign }
 }
+
+export const centerText = textAlign('center')
+
+export const justifyText = textAlign('justify')
