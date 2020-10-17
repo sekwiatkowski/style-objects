ce1e225322707134f9284a73a118d4ca9a166ea8
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-10-18 01:50:06 +0200

    line-height: added snugLineHeight

--- src/line-height.js
+++ src/line-height.js
@@ -4,6 +4,8 @@ export const lineHeight = absoluteStyle(lineHeight => ({ lineHeight }))
 
 export const tightLineHeight = lineHeight(1.0)
 
+export const snugLineHeight = lineHeight(1.25)
+
 export const normalLineHeight = lineHeight(1.5)
 
 export const relaxedLineHeight = lineHeight(1.75)
