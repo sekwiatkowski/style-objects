283a02da79f757db45827aa5e5dc393679e795c5
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-10-18 01:47:04 +0200

    line-height: removed veryRelaxedLineHeight, added tightLineHeight and changed relaxedLineHeight to 1.5

--- src/line-height.js
+++ src/line-height.js
@@ -2,8 +2,8 @@ import {absoluteStyle} from './base-unit'
 
 export const lineHeight = absoluteStyle(lineHeight => ({ lineHeight }))
 
+export const tightLineHeight = lineHeight(1.0)
+
 export const normalLineHeight = lineHeight(1.5)
 
-export const relaxedLineHeight = lineHeight(1.625)
-
-export const veryRelaxedLineHeight = lineHeight(1.75)
+export const relaxedLineHeight = lineHeight(1.75)
