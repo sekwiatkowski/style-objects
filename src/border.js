5031b22fce84c5a4f816041ded039ec58347fb73
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-10-19 04:55:28 +0200

    border: added function to set the width, style and color of individual sides of a border

--- src/border.js
+++ src/border.js
@@ -10,3 +10,30 @@ export const borderTop = createBorder('borderTop')
 export const borderRight = createBorder('borderRight')
 export const borderBottom = createBorder('borderBottom')
 export const borderLeft = createBorder('borderLeft')
+
+export function createBorderWidth(side) {
+    return width => ({side: width})
+}
+
+export const borderTopWidth = createBorderWidth('borderTop')
+export const borderRightWidth = createBorderWidth('borderRight')
+export const borderBottomWidth = createBorderWidth('borderBottom')
+export const borderLeftWidth = createBorderWidth('borderLeft')
+
+export function createBorderStyle(side) {
+    return width => ({side: width})
+}
+
+export const borderTopStyle = createBorderStyle('borderTop')
+export const borderRightStyle = createBorderStyle('borderRight')
+export const borderBottomStyle = createBorderStyle('borderBottom')
+export const borderLeftStyle = createBorderStyle('borderLeft')
+
+export function createBorderColor(side) {
+    return width => ({side: width})
+}
+
+export const borderTopColor = createBorderColor('borderTop')
+export const borderRightColor = createBorderColor('borderRight')
+export const borderBottomColor = createBorderColor('borderBottom')
+export const borderLeftColor = createBorderColor('borderLeft')
