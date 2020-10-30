aa2a3a0b59aec301233db0b4dd9de8ecb6f1537d
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-10-30 23:59:46 +0100

    transformation: added and export a scale function

--- src/transformation.js
+++ src/transformation.js
@@ -4,6 +4,10 @@ function rotate(degrees) {
     return `rotate(${degrees}deg)`
 }
 
+export function scale(factor) {
+    return `scale(${factor})`
+}
+
 export const rotateBy90Degrees = rotate(90)
 export const rotateBy180Degrees = rotate(180)
 
