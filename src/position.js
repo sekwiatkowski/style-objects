fc9160b372d196ec291e3d0d659f848988dea372
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-11-14 04:36:19 +0100

    position: exported top, right, bottom and left

--- src/position.js
+++ src/position.js
@@ -5,25 +5,25 @@ function position(position) {
 export const relative = position('relative')
 export const absolute = position('absolute')
 
-function top(left) {
+export function top(left) {
     return { left }
 }
 
 export const top0 = top('0')
 
-function right(right) {
+export function right(right) {
     return { right }
 }
 
 export const right0 = right('0')
 
-function bottom(bottom) {
+export function bottom(bottom) {
     return { bottom }
 }
 
 export const bottom0 = bottom('0')
 
-function left(left) {
+export function left(left) {
     return { left }
 }
 
