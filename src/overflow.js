0209864ae1e5ec358716e1ccab1ec9c29bb31c0d
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-11-14 23:28:21 +0100

    height: added maxHeight

---
+++ src/overflow.js
@@ -0,0 +1,10 @@
+function overflowX(overflowX) {
+    return { overflowX }
+}
+
+function overflowY(overflowY) {
+    return { overflowY }
+}
+
+export const overflowXAuto = overflowX('auto')
+export const overflowYAuto = overflowY('auto')
