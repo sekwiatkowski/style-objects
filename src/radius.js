02f62a9f0d14d3c18a6c20f55f8f3de35442108a
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2020-12-05 15:23:50 +0100

    radius: radii are now multiples of one sixteenth

--- src/radius.js
+++ src/radius.js
@@ -1,4 +1,4 @@
-import {eighthRemStyle} from './base-unit'
+import {sixteenthRemStyle} from './base-unit'
 import {merge} from 'standard-functions'
 
 function topRightRadius(borderTopRightRadius) {
@@ -29,20 +29,23 @@ function radius(borderRadius) {
     return {borderRadius}
 }
 
-const eighthRemRadius = eighthRemStyle(radius)
-const eighthRemLeftRadius = eighthRemStyle(leftRadius)
-const eighthRemRightRadius = eighthRemStyle(rightRadius)
+const sixteenthRemRadius = sixteenthRemStyle(radius)
+const sixteenthRemLeftRadius = sixteenthRemStyle(leftRadius)
+const sixteenthRemRightRadius = sixteenthRemStyle(rightRadius)
 
-export const leftRadius1 = eighthRemLeftRadius(1)
-export const leftRadius2 = eighthRemLeftRadius(2)
-export const leftRadius3 = eighthRemLeftRadius(3)
+export const leftRadius1 = sixteenthRemLeftRadius(1)
+export const leftRadius2 = sixteenthRemLeftRadius(2)
+export const leftRadius3 = sixteenthRemLeftRadius(3)
+export const leftRadius4 = sixteenthRemLeftRadius(4)
 
-export const rightRadius1 = eighthRemRightRadius(1)
-export const rightRadius2 = eighthRemRightRadius(2)
-export const rightRadius3 = eighthRemRightRadius(3)
+export const rightRadius1 = sixteenthRemRightRadius(1)
+export const rightRadius2 = sixteenthRemRightRadius(2)
+export const rightRadius3 = sixteenthRemRightRadius(3)
+export const rightRadius4 = sixteenthRemRightRadius(4)
 
-export const radius1 = eighthRemRadius(1)
-export const radius2 = eighthRemRadius(2)
-export const radius3 = eighthRemRadius(3)
+export const radius1 = sixteenthRemRadius(1)
+export const radius2 = sixteenthRemRadius(2)
+export const radius3 = sixteenthRemRadius(3)
+export const radius4 = sixteenthRemRadius(4)
 
 export const fullyRounded = radius('9999px')
