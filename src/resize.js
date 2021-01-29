4056cc33a73c25732f11f857a48e1a914f8b008e
Sebastian E. Kwiatkowski <sebastian.e.kwiatkowski@outlook.com>
2021-01-29 22:29:50 +0100

    Updated standard-functions dependency. resize: added constants to set the resize property

---
+++ src/resize.js
@@ -0,0 +1,11 @@
+function resize(resize) {
+    return { resize }
+}
+
+export const verticalResize = resize('vertical')
+
+export const horizontalResize = resize('horizontal')
+
+export const noResize = resize('none')
+
+export const resizeBoth = resize('both')
